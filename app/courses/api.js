bf8d7bfffa245218e74782babcd966fd819d33c5
Ian <ian@rbruns.com>
2024-10-02 14:33:33 -0600

    feat: inital fetch setup

--- app/courses/api.js
+++ app/courses/api.js
@@ -1,7 +1,5 @@
 import { get } from "../utils"
 
 export async function getCourses() {
-  return get(
-    "http://127.0.0.1:8090/api/collections/matches/records?page=1&perPage=30"
-  )
+  return get("http://127.0.0.1:8090/api/collections/matches/records")
 }
--- app/courses/page.js
+++ app/courses/page.js
@@ -4,10 +4,13 @@ export default async function CoursesPage() {
   const courses = await getCourses()
 
   return (
-    <div>
-      {courses?.items.map(course => (
-        <p key={course.id}>{course.matchName}</p>
-      ))}
-    </div>
+    <>
+      <h1>All Events</h1>
+      <div>
+        {courses?.items.map(course => (
+          <p key={course.id}>{course.matchName}</p>
+        ))}
+      </div>
+    </>
   )
 }
