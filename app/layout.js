5fcd51027b3af0fabed2b960c51aff6edd5012fd
Ian <ian@rbruns.com>
2024-10-02 15:11:47 -0600

    feat: initial header nav style

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import Link from "next/link"
 
+import "./globals.css"
+
 export default function Layout({ children }) {
   return (
     <html>
--- app/page.js
+++ app/page.js
@@ -1,8 +1,16 @@
 export default function HomePage() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p>Some Content</p>
+      <h1>Golf Stats</h1>
+      <p>Welcome to Golf Stats!</p>
+      <p>This is an app that will allow you to:</p>
+      <ul>
+        <li>
+          Add golf matches you have played with you and your friend&#39;s stats
+        </li>
+        <li>See a graph comparing your stats against par</li>
+      </ul>
+      <p>This a NextJS Project assisted by Pocketbase and testing-library</p>
     </div>
   )
 }
